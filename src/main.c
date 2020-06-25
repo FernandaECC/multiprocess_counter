11a7b7f6a0b6aa60fa12bda917ffd0f64d38cd28
FernandaECC <48841597+FernandaECC@users.noreply.github.com>
2020-06-25 12:49:37 -0300

    Funcionando

--- src/main.c
+++ src/main.c
@@ -36,17 +36,21 @@ int main() {
   int num_processos = 0;
   
   /*Tratamento e armazenamento da sequência de números (input)*/
-  int i=0, j=0;
-  int array[100];
-  int array_f0[100], array_f1[100], array_f2[100], array_f3[100];
+  int i=0, j=0, v=0; //variaveis utilizadas nos for's
+  
+  int array[100]; //vetor da entrada
+  int array_f0[100], array_f1[100], array_f2[100], array_f3[100]; 
+  //um vetor para cada processo filho, distribuindo assim a entrada entre os processos filhos ---> cada processo filho sabera previamente quais numeros ira processar
+
   char temp;
   do {
       scanf("%d%c", &array[i], &temp);
       i++;
       } while(temp != '\n');
-  //printf("numero de elementos: %d\n", i);  
-  int v = 0;
-  int w0=0, w1=0, w2=0, w3=0;  
+      
+        
+/*distribuicao dos numeros recebidos na entrada para cada vetor de cada processo filho*/        
+  int w0=0, w1=0, w2=0, w3=0; //variaveis auxiliadoras na atribuicao  
   for(j=0; j<i; j++) {
  	v = j%4;
  	if(v == 0) {array_f0[w0] = array[j];
@@ -71,15 +75,14 @@ int main() {
   
  
   
-  //###########
-  unsigned int p;
-  unsigned int aux2;
+  
+  unsigned int p; // variavel que indicara se o numero eh primo (p = 1) ou nao (p = 0)
 
-  /* Definir flags de protecao e visibilidade de memoria */
+  //definir flags de protecao e visibilidade de memoria 
   int protection = PROT_READ | PROT_WRITE;
   int visibility = MAP_SHARED | MAP_ANON;
 
-  /* Criar area de memoria compartilhada */
+  /* Criar area de memoria compartilhada --> variavel que ira contar quantos numeros primos ha na entrada */
   int *count;
   count = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
 
@@ -88,35 +91,34 @@ int main() {
   /*inicializando...*/
 
   (*count) = 0;
-  int k0, k1, k2, k3;
-  int g;
-  for (int k=0; k<num_processos; k++) {
+  
+  int k0, k1, k2, k3; //variaveis que armazenarao os IDs de cada processo filho
+  int g; //variavel auxiliar
+  
+  for (int k=0; k<num_processos; k++) { //gerando ate quatro processos filhos
     filho[k] = fork();
     
+    //obtendo o ID de cada processo filho
     if (filho[k] == 0) {
     	if(k == 0) {k0 = getpid(); //obtem o ID do filho 0
-    	//printf("ID de %d: %i\n", k, getpid());
     	}
    
     	else if(k == 1) {k1 = getpid(); //obtem o ID do filho 1
-    	//printf("ID de %d: %i\n", k, getpid());
     	}	
     
     	else if(k == 2) {k2 = getpid(); //obtem o ID do filho 2
-    	//printf("ID de %d: %i\n", k, getpid());
     	}	
     
     	else if(k == 3) {k3 = getpid(); //obtem o ID do filho 3
-    	//printf("ID de %d: %i\n", k, getpid());
     	}	
     	
     	
-    	
+    	//cada processo filho ira verificar se o seu conjunto de numeros previamente armazenados em vetores sao primos ou nao
+    	// se for primo, incrementa a variavel COUNT compartilhada entre processos por meio de mmap()
     	if(getpid() == k0) { 
     		for(g=0; g< w0; g++){
       			p = primo(array_f0[g]);
       			if (p == 1) {(*count)++ ;}
-      			//printf("Filho %d achou primo(%d)=%d\n", 0, array_f0[g] , p);
     			}
     	}
     	
@@ -124,7 +126,6 @@ int main() {
     		for(g=0; g< w1; g++){
       			p = primo(array_f1[g]);
       			if (p == 1) {(*count)++ ;}
-      			//printf("Filho %d achou primo(%d)=%d\n", 1, array_f1[g] , p);
     			}
     	}
     	
@@ -132,7 +133,6 @@ int main() {
     		for(g=0; g< w2; g++){
       			p = primo(array_f2[g]);
       			if (p == 1) {(*count)++ ;}
-      			//printf("Filho %d achou primo(%d)=%d\n", 2, array_f2[g] , p);
     			}
     	}
     	
@@ -140,28 +140,19 @@ int main() {
     		for(g=0; g< w3; g++){
       			p = primo(array_f3[g]);
       			if (p == 1) {(*count)++ ;}
-      			//printf("Filho %d achou primo(%d)=%d\n", 3, array_f3[g] , p);
     			}
     	}    	    	
     	
     	
-    	
-    			
-      /* Esta parte do codigo executa no processo filho */
-      
-      
-      
-      
-      
-      //printf("count = %d\n", (*count));
       exit(0);
     }
   }
 
-  //printf("Todos os filhos foram gerados. Esperando...\n");
+  //espera cada processo filho terminar de executar
   for (int k=0; k<N_PROCESSOS; k++) {
     waitpid(filho[k], NULL, 0);
   }
 
-  printf("%d\n", *count);
+  printf("%d\n", *count); //imprime na tela o resultado final da operacao
   return 0;
+}
